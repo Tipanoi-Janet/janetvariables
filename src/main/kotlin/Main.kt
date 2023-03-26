1feb0722e8fbc7c7a3da6930a42a79aac1ad84eb
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-03-26 18:30:39 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    var name = "janet"
+    var girls = 34.02
+    var position = 44787090
+    var boyspopulation = 6700000000
+    var worldpopulation = 34.0
+
+    val text = "PB780KMPOT you have received 1000 from james njala"
+    val weight = 5.6
+    val receivedbag = false
+}
